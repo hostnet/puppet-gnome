7219a84097f6bea91657999d877d48ba95e39e66
Erik <edekker@hostnet.nl>
2019-04-02 13:51:11 +0200

    Don't expect dbus in spec test

--- spec/defines/gsettings_spec.rb
+++ spec/defines/gsettings_spec.rb
@@ -146,7 +146,7 @@ describe 'gnome::gsettings' do
 
     it do
       should contain_exec('change-org.rspec.testing-/desktop/gnome/rspec/testing').with({
-        'command' => "dbus-launch gsettings set org.rspec.testing /desktop/gnome/rspec/testing /usr/rspec/testing",
+        'command' => "gsettings set org.rspec.testing /desktop/gnome/rspec/testing /usr/rspec/testing",
         'path'    => '/usr/bin',
         'user'    => 'testuser',
       })
